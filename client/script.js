fee09a14f25f6b7120e4ebfa3ce256167620909b
linda8850 <lijia9988@qq.com>
2023-04-23 16:50:44 +0800

    修改后端接口

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv)
 
-  const response = await fetch('https://codex-xz7q.onrender.com/', {
+  const response = await fetch('https://codex01.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
